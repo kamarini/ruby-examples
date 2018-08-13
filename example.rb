8b1c09c7c9a8bd883aebb94e04b54a0258f808bd
Kátia Andréa Marini Marson <kamarini7@gmail.com>
2018-08-12 22:30:36 -0300

    Criação da Calculadora

--- example.rb
+++ example.rb
@@ -1,1 +1,22 @@
-puts 'Hello World'
+puts "Calculadora"
+puts "Digite um número: "
+value1 = gets.to_f
+puts "Digite a operação que deseja realizar (+, -, *, /): "
+operation = gets.chomp
+puts "Digite outro número: "
+value2 = gets.to_f
+
+case operation
+when '+'
+  result = value1 + value2
+when '-'
+  result = value1 - value2
+when '*'
+  result = value1 * value2
+when '/'
+  result = value1 / value2
+else
+  puts "Operação invalida."
+end
+
+puts "O resultado é: #{result}" if result
